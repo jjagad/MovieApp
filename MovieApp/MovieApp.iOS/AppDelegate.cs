87e7dcfff257194a9dcc3f18dd1cee850ca65122
Jash Jagad <jashjagad@gmail.com>
2019-09-18 22:28:00 +0530

    Changes in App Delegate and icon additions for iOS

--- MovieApp/MovieApp.iOS/AppDelegate.cs
+++ MovieApp/MovieApp.iOS/AppDelegate.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using Acr.UserDialogs;
+using FFImageLoading.Forms.Platform;
 using Foundation;
 using UIKit;
 
@@ -23,6 +24,9 @@ namespace MovieApp.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            CachedImageRenderer.Init();
+            Rg.Plugins.Popup.Popup.Init();
+           
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
